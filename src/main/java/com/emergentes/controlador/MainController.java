5a91fc5d044fd9551a07d4da1eecce1245b54f82
Noonexyz1 <aries-d2github@hotmail.com>
2023-05-09 12:53:36 -0400

    Implementacio de los jsp

--- src/main/java/com/emergentes/controlador/MainController.java
+++ src/main/java/com/emergentes/controlador/MainController.java
@@ -1,4 +1,3 @@
-
 package com.emergentes.controlador;
 
 import com.emergentes.modelo.Libro;
@@ -8,52 +7,113 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet(name = "MainController", urlPatterns = {"/MainController"})
 public class MainController extends HttpServlet {
 
-  
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String op;
-        op = (request.getParameter("op") != null)? request.getParameter("op"): "list";
-        ArrayList<Libro> listaDeLibros = new ArrayList<>();
-        ConexionBD canal = new ConexionBD();
-        Connection conn = canal.conectar();
-        PreparedStatement ps;
-        ResultSet rs;
-        
-        if (op.equals("list")) {
-            String sql = "SELECT * FROM LIBROS";
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();
-        }
-        if (op.equals("nuevo")) {
-            
-        }
-        
-        if (op.equals("eliminar")) {
+        try {
 
+            String op;
+            op = (request.getParameter("op") != null) ? request.getParameter("op") : "list";
+            ArrayList<Libro> listaDeLibros = new ArrayList<>();
+            ConexionBD canal = new ConexionBD();
+            Connection conn = canal.conectar();
+            PreparedStatement ps;
+            ResultSet rs;
+
+            if (op.equals("list")) {
+                String sql = "SELECT * FROM LIBROS";
+                ps = conn.prepareStatement(sql);
+                rs = ps.executeQuery();
+
+                while (rs.next()) {
+                    Libro lib = new Libro();
+                    lib.setId(rs.getInt("id"));
+                    lib.setIsbn(rs.getString("isbn"));
+                    lib.setTitulo(rs.getString("titulo"));
+                    lib.setCategoria(rs.getString("categoria"));
+
+                    listaDeLibros.add(lib);
+                }
+
+                request.setAttribute("lista", listaDeLibros);
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+
+            }
+            if (op.equals("nuevo")) {
+                Libro li = new Libro();
+                request.setAttribute("lib", li);
+                request.getRequestDispatcher("editar.jsp").forward(request, response);
+
+            }
+
+            if (op.equals("eliminar")) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                String sql = "DELETE FROM LIBROS WHERE ID = ?";
+                ps = conn.prepareStatement(sql);
+                ps.setInt(1, id);
+
+                ps.executeUpdate();
+                response.sendRedirect("MainController");
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-    }
+        try {
 
- 
+            int id = Integer.parseInt(request.getParameter("id"));
+            String isbn = request.getParameter("isbn");
+            String titulo = request.getParameter("titulo");
+            String categoria = request.getParameter("categoria");
+
+            Libro lib = new Libro();
+            lib.setId(id);
+            lib.setIsbn(isbn);
+            lib.setTitulo(titulo);
+            lib.setCategoria(categoria);
+
+            ConexionBD canal = new ConexionBD();
+            Connection conn = canal.conectar();
+
+            PreparedStatement ps;
+
+            if (id == 0) {
+                String sql = "INSERT INTO LIBROS(isbn, titulo, categoria) VALUES(?,?,?)";
+                ps = conn.prepareCall(sql);
+
+                ps.setString(1, lib.getIsbn());
+                ps.setString(2, lib.getTitulo());
+                ps.setString(3, lib.getCategoria());
+
+                ps.executeUpdate();
+
+                response.sendRedirect("MainController");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Error en SQL " + ex.getMessage());
+        }
+
+    }
 
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,42 @@
+<%@page import="java.util.ArrayList"%>
+<%@page import="com.emergentes.modelo.Libro"%>
+<%
+    ArrayList<Libro> lista = (ArrayList<Libro>) request.getAttribute("lista");
+%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Listado de Libros</h1>
+        <p><a href="MainController?op=nuevo"></a></p>
+        <table border="1">
+            <tr>
+                <th>Id</th>
+                <th>ISBN</th>
+                <th>Titulo</th>
+                <th>CAtegoria</th>
+                <th></th>
+            </tr>
+            <c:forEach var="item" items="${lista}">
+                <tr>
+                    <td>${item.id}</td>
+                    <td>${item.isbn}</td>
+                    <td>${item.titulo}</td>
+                    <td>${item.categoria}</td>
+                    <td>
+                        <a href="MainController?op=eliminar&id=${item.id}" onclick="return(confirm('Estas seguro de eliminar??'))">Eliminar</a>
+                    </td>
+                </tr>
+            </c:forEach>
+            
+            
+            
+        </table>
+        
+        
+    </body>
+</html>
